bf0a96f2570c3b5a5c8366ac0da7f6abe98d0906
labylei <57024733+labylei@users.noreply.github.com>
2020-06-18 21:58:23 +0800

    完成商品站页面

--- mimall/src/pages/product.vue
+++ mimall/src/pages/product.vue
@@ -1,10 +1,228 @@
 <template >
-    <div>
-      product
+    <div class="product">
+      <product-param :title="product.name">
+        <template v-slot:buy>
+          <button class="btn" @click="buy()">立即购买</button>
+        </template>
+      </product-param>
+      <div class="body">
+        <div class="item-bg">
+          <h2>{{product.name}}</h2>
+          <h3>{{product.subtitle}}</h3>
+          <p>
+            <a href="" id="">全球首款双频 GP</a>
+            <span>|</span>
+            <a href="" id="">骁龙845</a>
+            <span>|</span>
+            <a href="" id="">AI 变焦双摄</a>
+            <span>|</span>
+            <a href="" id="">红外人脸识别</a>
+          </p>
+        <span>￥<em>{{product.price}}</em></span>
+        </div>
+        <div class="container">
+          <div class="item-detail"></div>
+        </div>
+        <div class="item-bg-2"></div>
+        <div class="item-swiper">
+          <swiper :options="swiperOption">
+          <swiper-slide><img src="/imgs/product/gallery-2.png" alt=""></swiper-slide>
+          <swiper-slide><img src="/imgs/product/gallery-3.png" alt=""></swiper-slide>
+          <swiper-slide><img src="/imgs/product/gallery-4.png" alt=""></swiper-slide>
+          <swiper-slide><img src="/imgs/product/gallery-5.jpg" alt=""></swiper-slide>
+          <swiper-slide><img src="/imgs/product/gallery-6.jpg" alt=""></swiper-slide>
+          <!-- Optional controls -->
+          <div class="swiper-pagination"  slot="pagination"></div>
+          </swiper>
+        </div>
+        <div class="item-video">
+          <h2>60帧超慢动作摄影<br />慢慢回味每一个瞬间的精彩</h2>
+          <p>后置960帧电影般超慢动作视频，将眨眼间的美妙展现得淋漓尽致！<br/>更能AI 精准分析视频内容，15个场景智能匹配背景音效。</p>
+          <div class="video-bg" @click="showSlide=true"></div>
+          <div class="video-box">
+            <div class="overlay" v-if="showSlide"></div>
+            <div class="video" :class="{'slide':showSlide}">
+              <span class="icon-close" @click="showSlide=false"></span>
+              <video src="/imgs/product/video.mp4" muted autoplay controls="controls"></video>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 <script>
+import ProductParam from './../components/ProductParam';
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
 export default {
-    name:'product'
+    name:'product',
+    components:{
+      ProductParam,
+      Swiper,
+      SwiperSlide
+    },
+    data(){
+      return{
+        showSlide:false,//控制动画效果
+        product:{},//商品信息
+        swiperOption:{
+          autoplay:true,
+          slidesPerView:3,
+          spaceBetween: 30,
+          freeMode: true,
+          loop:true,
+          pagination: {
+            el: '.swiper-pagination',
+            clickable :true,
+          }
+        }
+    }
+  },
+  mounted(){
+    this.getProductInfo();
+  },
+  methods:{
+    getProductInfo(){
+      let id = this.$route.params.id;
+      this.axios.get(`/products/${id}`).then((res)=>{
+        this.product = res;
+      })
+    },
+    buy(){
+      let id = this.$route.params.id;
+      this.$router.push(`/detail/${id}`);
+    },
+  }
 }
 </script>
+<style lang="scss">
+@import './../assets/scss/mixin.scss';
+  .product{
+    .btn{
+    margin-left: 10px;
+    z-index: 10;
+   }
+  .body{
+    position: relative;
+    .item-bg{
+      background: url('/imgs/product/product-bg-1.png') no-repeat center;
+      height: 718px;
+      background-size: 1920px 718px;
+      text-align: center;
+      h2{
+          font-size:80px;
+          padding-top:55px;
+        }
+        h3{
+          font-size:24px;
+          letter-spacing: 10px;
+        }
+        p{
+          margin-top:21px;
+          margin-bottom:40px;
+          a{
+            font-size:16px;
+            color:#333333;
+          }
+          span{
+            margin:0 15px;
+          }
+        }
+        span{
+          font-size:30px;
+          color:#333333;
+          em{
+          font-style:normal;
+          font-size:38px;
+          }
+        }
+        
+    }
+
+    .container{
+      .item-detail{
+        background: url('/imgs/product/product-bg-2.png') no-repeat center;
+        height: 480px;
+        background-size:1226px 397px;
+      }
+    }
+    .item-bg-2{
+        background: url('/imgs/product/product-bg-3.png') no-repeat center;
+        height: 638px;
+        background-size: 1920px 638px;
+        margin-top: 0;
+    }
+    .item-swiper{
+        margin:36px auto 52px;
+        .desc{
+          font-size:18px;
+          color:#333333;
+          text-align:center;
+        }
+        img{
+          width:100%;
+        }
+    }
+    .item-video{
+      height: 1044px;
+      background-color: #070708;
+      margin-bottom: 76px;
+      color: #ffffff;
+      text-align: center;
+      h2{
+        font-size: 60px;
+        padding-top: 82px;
+        margin-bottom: 47px;
+      }
+      p{
+        font-size: 24px;
+        margin-bottom: 58px;
+      }
+      .video-bg{
+        background: url('/imgs/product/gallery-1.png') no-repeat center;
+        background-size: cover;
+        width: 1226px;
+        height: 540px;
+        margin: 0 auto 120px;
+        cursor: pointer;
+      }
+      .video-box{
+        .overlay{
+          @include position(fixed);
+          background-color: #333333;
+          opacity: 0.4;
+          z-index: 10;
+        }
+        .video{
+          position: fixed;
+          top: -50%;
+          left: 50%;
+          transform: translate(-50%,-50%);
+          z-index: 10;
+          width: 1000px;
+          height: 536px;
+          opacity: 0;
+          transition: all .6s;
+          &.slide{
+            top:50%;
+            opacity: 1;
+          }
+          .icon-close{
+            position: absolute;
+            top: 20px;
+            right: 20px;
+            cursor: pointer;
+            z-index: 10;
+            @include bgImg(20px,20px,'/imgs/icon-close.png');
+          }
+          video{
+            width: 100%;
+            height: 100%;
+            object-fit: cover;//视频内容覆盖整个窗口
+            outline: none;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
