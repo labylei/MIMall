3de5d620088190fb1c81c26f1c7abd9afbba7235
labylei <57024733+labylei@users.noreply.github.com>
2020-07-22 21:16:48 +0800

    完成订单结算

--- mimall/src/components/Modal.vue
+++ mimall/src/components/Modal.vue
@@ -5,17 +5,17 @@
         <div class="modal-dialog">
             <div class="modal-header">
                 <span>{{title}}</span>
-                <a href="javascript:;" class="icon-close" @click="$emit('concel')"></a>
+                <a href="javascript:;" class="icon-close" @click="$emit('cancel')"></a>
             </div>
             <div class="modal-body">
                 <slot name="body"></slot>
             </div>
             <div class="modal-footer">
                 <a href="javascript:;" class="btn" v-if="btnType==1" @click="$emit('submit')">{{sureText}}</a>
-                <a href="javascript:;" class="btn" v-if="btnType==2" @click="$emit('concel')">{{cancelText}}</a>
+                <a href="javascript:;" class="btn" v-if="btnType==2" @click="$emit('cancel')">{{cancelText}}</a>
                 <div class="btn-group" v-if="btnType==3">
                     <a href="javascript:;" class="btn" @click="$emit('submit')">{{sureText}}</a>
-                    <a href="javascript:;" class="btn" @click="$emit('concel')">{{cancelText}}</a>
+                    <a href="javascript:;" class="btn" @click="$emit('cancel')">{{cancelText}}</a>
                 </div>
             </div>
         </div>
